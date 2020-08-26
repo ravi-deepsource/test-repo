44aeec7b4cc25074a447ea1a215b00301bcfd0ee
Ravi Teja Gannavarapu <69030640+ravi-deepsource@users.noreply.github.com>
2020-08-26 20:30:16 +0530

    Create test.rb

---
+++ test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# This is a test shit
+
+a = {'hello' => "world", "testing" => BigDecimal.new(123.456, 3)}
+puts a
+
+begin
+  do_something
+end while some_condition
+
+b = [1, 2, 3, 4, 5]
+puts b
+
+OpenSSL::Cipher::AES.new(128, :GCM)
+
+# shit happens
