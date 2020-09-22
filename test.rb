2a6536c548899901d3eeadf60fb0d08c5bf8493d
Ravi Teja Gannavarapu <69030640+ravi-deepsource@users.noreply.github.com>
2020-09-23 01:17:44 +0530

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,1 +1,42 @@
+# frozen_string_literal: true
+# encoding: ascii
 
+a = {'hello' => 'world', "testing" => BigDecimal.new(123.456, 3)}
+puts a
+
+b = [1, 2, 3, 4, 5]
+puts b
+
+%i[:foo :bar]
+
+begin
+  do_something
+end while unless true != false
+
+x < y < z
+10 <= x <= 20
+
+begin
+  bar
+rescue
+  baz
+end
+
+def some_method(bar)
+  puts "Hello"
+end
+
+
+def some_method
+  return
+  do_something
+end
+
+OpenSSL::Cipher::AES.new(128, :GCM)
+
+OpenSSL::Cipher::AES.new(128, :GCM)
+
+OpenSSL::Cipher::AES.new(128, :GCM)
+
+def some_method
+end
