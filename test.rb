5f2b2376d5bf284f1276c93115a82d468e6c23fc
Ravi Teja Gannavarapu <69030640+ravi-deepsource@users.noreply.github.com>
2020-11-05 18:45:47 +0530

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,48 +1,5 @@
-# encoding: ascii
 # frozen_string_literal: true
 
-a = {'hello' => 'world', 'testing' => BigDecimal(123.456, 3)}
-puts a
-
-b = [1, 2, 3, 4, 5]
+a = 15
+b = 17
 puts b
-
-%i[foo bar]
-
-loop do
-  do_something
-break unless true != false
-end
-
-x < y && y < z
-10 <= x && x <= 20
-
-begin
-  bar
-rescue
-  baz
-end
-
-def some_method(_bar)
-  puts 'Hello'
-end
-
-
-def some_method
-  return
-  do_something
-end
-
-class Dirty
-  def m(a,b,c)
-    puts a,b
-  end
-end
-
-OpenSSL::Cipher.new('aes-128-gcm')
-
-OpenSSL::Cipher.new('aes-128-gcm')
-
-OpenSSL::Cipher.new('aes-128-gcm')
-
-def some_method; end
