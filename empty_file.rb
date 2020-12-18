23d8079a2534432c74c883779b8962fa9b82a1a7
Ravi Teja Gannavarapu <69030640+ravi-deepsource@users.noreply.github.com>
2020-12-18 21:22:11 +0530

    Update empty_file.rb

--- empty_file.rb
+++ empty_file.rb
@@ -1,25 +1,4 @@
 # frozen_string_literal: true
 
-begin
-  do_something
-rescue Exception
-  do_something_else
-end
-
-begin
-  do_something
-rescue Exception
-  do_something_else
-end
-
-begin
-  do_something
-rescue Exception
-  do_something_else
-end
-
-AUTOFIX ||= 'does_this_even_work?'
-AUTOFIX = 'test'
-
-fix ||= 'yes'
-fix = 'no'
+open(something)
+URI.open(something)
