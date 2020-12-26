34b24408a4159254c05b653432a4c98c5e24d1aa
Ravi Teja Gannavarapu <69030640+ravi-deepsource@users.noreply.github.com>
2020-12-26 23:01:55 +0530

    Update empty_file.rb

--- empty_file.rb
+++ empty_file.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
-open(something)
-URI.open(something)
+some_variable ||= File.read('test.txt')
+some_variable = File.read('test2.txt')
 
-BigDecimal.new("123")
+SOME_CONSTANT = 'some_value'
+SOME_CONSTANT = 'some_other_value'
+
+IO.select([io], [], [], timeout)
+
+def display_cop_names?
+  return true if debug?
+  return false if options[:display_cop_names] == false
+  return true if options[:display_cop_names]
+  return false if options[:format] == 'json'
+  return true if options[:display_cop_names].any?
+
+  config.for_all_cops['DisplayCopNames']
+end
+
+a = 3
+b = 1 + a
