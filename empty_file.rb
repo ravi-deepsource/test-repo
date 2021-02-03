bc1ecd6230fa28264782390f62739173c64797bb
Ravi Teja Gannavarapu <69030640+ravi-deepsource@users.noreply.github.com>
2021-02-03 23:17:25 +0530

    Update empty_file.rb

--- empty_file.rb
+++ empty_file.rb
@@ -1,33 +1,14 @@
 # frozen_string_literal: true
 
-some_variable ||= File.read('test.txt')
-some_variable = File.read('test2.txt')
-
-SOME_CONSTANT = 'some_value'
-SOME_CONSTANT = 'some_other_value'
-
-IO.select([io], [], [], timeout)
-IO.select([], [io], [], timeout)
-
-def display_cop_names?
-  return true if debug?
-  return false if options[:display_cop_names] == false
-  return true if options[:display_cop_names]
-  return false if options[:format] == 'json'
-  return true if options[:display_cop_names].any?
-
-  config.for_all_cops['DisplayCopNames']
-end
-
-a = 3
-b = 1 + a
-
-return 1, 2, 3
-
-p = 1
-
-begin
+if something
   do_something
-rescue Exception
-  do_nothing
+  do_something_else
 end
+
+
+case some_var
+when 1
+  do_something
+end
+
+ip_addr = '122.132.11.133'
