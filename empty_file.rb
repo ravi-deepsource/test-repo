977335808af1c6c9c94826ff1cc87cf85e311fe4
Ravi Teja Gannavarapu <69030640+ravi-deepsource@users.noreply.github.com>
2021-02-15 23:25:09 +0530

    Update empty_file.rb

--- empty_file.rb
+++ empty_file.rb
@@ -1,17 +1,4 @@
 # frozen_string_literal: true
 
-if something
-  do_something
-  do_something_else
-end
-
-
-case some_var
-when 1
-  do_something
-end
-
-ip_addr = '122.132.11.133'
-
 test_string = 'Cya!'
 puts test_string
